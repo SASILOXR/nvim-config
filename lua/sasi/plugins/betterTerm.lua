296c9f8de1990ce3761904e2106dd31accf9371a
ppjtg <ppjtg@qq.com>
2024-08-14 23:29:16 +0800

    updata a lit

--- lua/sasi/plugins/betterTerm.lua
+++ lua/sasi/plugins/betterTerm.lua
@@ -13,5 +13,8 @@ return {
 		set({ "n", "t" }, ";", betterTerm.open, { desc = "open term" })
 		set("n", "<leader>tt", betterTerm.select, { desc = "select term" })
 		set("t", "<ESC>", "<C-\\><C-n>", { desc = "exit term mode" })
+		vim.api.nvim_create_user_command("Openterm", function(opts)
+			betterTerm.open(opts.fargs[1])
+		end, { nargs = 1 })
 	end,
 }
